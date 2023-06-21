e56d4d1947780c8f6a19b5f7c886156ee5fbe8de
SoniyaPawar1681 <136687931+SoniyaPawar1681@users.noreply.github.com>
2023-06-21 20:14:01 +0530

    Create Program48.java

---
+++ Program48.java
@@ -0,0 +1,32 @@
+package soniya;
+
+public class demo1 
+{
+
+	public static void main(String[] args) 
+	{
+	   int marks=57;
+	   if(marks>=85)
+	   {
+		   System.out.println("A Grade");
+	   }
+	   else if(marks>=75)
+	   {
+		   System.out.println("B Grade"); 
+	   }
+	   else if(marks>=65)
+	   {
+		   System.out.println("c Grade");
+	   }
+	   else if(marks>=55)
+	   {
+		   System.out.println("D Grade");
+	   }
+	   else 
+	   {
+		   System.out.println("Failed Exam");
+	   }
+
+	}
+
+}
