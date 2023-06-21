7c458a03f8ec3ae98abf60472f965d35e02c136f
SoniyaPawar1681 <136687931+SoniyaPawar1681@users.noreply.github.com>
2023-06-21 18:45:33 +0530

    Create Program14.java

---
+++ Program14.java
@@ -0,0 +1,25 @@
+package Oops_Concept;
+ class Student
+ {
+	 public Student()
+	 {
+		System.out.println("welcome");
+	 }
+
+	public void dis() 
+	{
+		System.out.println("Fortune Cloud Technology");
+		
+	}
+ }
+public class Constructor 
+{
+
+	public static void main(String[] args)
+	{
+		Student s1=new Student();
+        s1.dis();
+	}
+
+}
+P
