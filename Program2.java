826bee0a2f044225b5cfaedfc4d94561b477fe5f
SoniyaPawar1681 <136687931+SoniyaPawar1681@users.noreply.github.com>
2023-06-21 18:36:13 +0530

    Create Program2.java

---
+++ Program2.java
@@ -0,0 +1,19 @@
+package Basic;
+
+public class Even_odd 
+{
+
+	public static void main(String[] args) 
+	{
+		int num=6;
+	    if(num%2==0)
+	    {
+	    	System.out.println("Even Number");
+	    }
+	    else
+	    {
+	    	System.out.println("odd Number");
+	    }
+	}
+
+}
