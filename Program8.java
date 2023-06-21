3b7ac7434551d48e595f89f08a7a753cb2fb125d
SoniyaPawar1681 <136687931+SoniyaPawar1681@users.noreply.github.com>
2023-06-21 18:41:19 +0530

    Create Program8.java

---
+++ Program8.java
@@ -0,0 +1,21 @@
+package Pattern;
+import java.util.*;
+public class Inverted_Half_Pyramid1 
+{
+
+	public static void main(String[] args) 
+	{
+	  Scanner sc=new Scanner(System.in);
+	  int n=sc.nextInt();
+	  
+	  for(int i=n;i>=1;i--)
+	  {
+		  for(int j=1;j<=i;j++)
+		  {
+			System.out.print("*"); 
+		  }
+		  System.out.println();
+	  }
+	}
+
+}
