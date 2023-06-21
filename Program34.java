e1899c4f65887d56ea38c218da3e48907d305c33
SoniyaPawar1681 <136687931+SoniyaPawar1681@users.noreply.github.com>
2023-06-21 20:00:20 +0530

    Create Program34.java

---
+++ Program34.java
@@ -0,0 +1,18 @@
+package collectionfreamework1;
+import java.util.*;
+public class Hset 
+{
+
+	public static void main(String[] args) 
+	{
+	 HashSet<Integer> h1=new HashSet<Integer>();
+	 h1.add(34);
+	 h1.add(56);
+	 h1.add(67);
+	 h1.add(23);
+	 
+	 System.out.println(h1);
+
+	}
+
+}
