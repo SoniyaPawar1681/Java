06c26eb35104df971526ba7c07e5c4b2eac43fe6
SoniyaPawar1681 <136687931+SoniyaPawar1681@users.noreply.github.com>
2023-06-21 20:15:33 +0530

    Create Program50.java

---
+++ Program50.java
@@ -0,0 +1,17 @@
+package even;
+
+public class table_print 
+{
+
+	public static void main(String[] args) 
+	{
+		int i,ans,n=2;
+		for(i=1;i<=10;i++)
+		{
+			ans=n*i;
+			System.out.println(ans);
+		}
+
+	}
+
+}
