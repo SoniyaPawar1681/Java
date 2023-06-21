d0660e5913cd96cb1c5600b5525de34ad53be7e5
SoniyaPawar1681 <136687931+SoniyaPawar1681@users.noreply.github.com>
2023-06-21 20:02:09 +0530

    Create Program36.java

---
+++ Program36.java
@@ -0,0 +1,20 @@
+package collectionfreamework1;
+import java.util.*;
+public class Map_interface 
+{
+
+	public static void main(String[] args) 
+	{
+		Map<Integer,String> m1=new HashMap<Integer,String>();
+		m1.put(12,"Soniya");
+		m1.put(23,"Mayuri");
+		m1.put(45,"Priyanka");
+		m1.put(14,"Ankita");
+		m1.put(67,"vrushalee");
+		m1.put(null,"null");
+		
+		System.out.println(m1);
+
+	}
+
+}
