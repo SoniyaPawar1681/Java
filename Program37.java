588799dbcd947f1b227e93039cb9b21992fd7b3f
SoniyaPawar1681 <136687931+SoniyaPawar1681@users.noreply.github.com>
2023-06-21 20:03:13 +0530

    Create Program37.java

---
+++ Program37.java
@@ -0,0 +1,18 @@
+package collectionfreamework1;
+import java.util.*;
+public class Treeset 
+{
+
+	public static void main(String[] args) 
+	{
+		TreeSet<String> t1=new TreeSet<String>();
+		t1.add("Soniya");
+		t1.add("Aniket");
+		t1.add("Priyanka");
+		t1.add("Mayuri");
+		t1.add("Abhishek");
+		System.out.println(t1);
+
+	}
+
+}
