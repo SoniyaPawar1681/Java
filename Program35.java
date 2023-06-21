d9d02538722ab013d8ed6db27eeb018dd47cf9d7
SoniyaPawar1681 <136687931+SoniyaPawar1681@users.noreply.github.com>
2023-06-21 20:01:11 +0530

    Create Program35.java

---
+++ Program35.java
@@ -0,0 +1,18 @@
+package collectionfreamework1;
+import java.util.*;
+public class linkedHset 
+{
+
+	public static void main(String[] args) 
+	{
+	 LinkedHashSet<Integer>	h1=new LinkedHashSet<Integer>();
+	 h1.add(56);
+	 h1.add(43);
+	 h1.add(23);
+	 h1.add(89);
+	 h1.add(87);
+	 System.out.println(h1);
+
+	}
+
+}
