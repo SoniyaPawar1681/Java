fddacc70e3a1402ede194ec8d7b213129e9b18c4
SoniyaPawar1681 <136687931+SoniyaPawar1681@users.noreply.github.com>
2023-06-21 20:06:29 +0530

    Create Program40.java

---
+++ Program40.java
@@ -0,0 +1,20 @@
+package excepion_handling1;
+
+public class arithmatic_
+{
+
+	public static void main(String[] args) 
+	{
+		try
+		{
+		int a=10,b=0,c;
+		c=a/b;
+		System.out.println(c);
+		}
+		catch(Exception e)
+		{
+			System.out.println("arithmatic exception");
+		}
+	}
+
+}
