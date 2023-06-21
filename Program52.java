14f75af3856dfc7c07654a09f4fbc760ed954454
SoniyaPawar1681 <136687931+SoniyaPawar1681@users.noreply.github.com>
2023-06-21 20:18:01 +0530

    Create Program52.java

---
+++ Program52.java
@@ -0,0 +1,23 @@
+package constructor;
+class student
+{
+	public student()
+	{
+		System.out.println("Welcome");
+	}
+	public void disp()
+	{
+		System.out.println("Hello India");
+	}
+}
+public class default_constructor
+{
+
+	public static void main(String[] args) 
+	{
+	student s1=new student();
+	s1.disp();		
+
+	}
+
+}
