34ac1523fb6f149765c0c81c0ec997d650f47e78
SoniyaPawar1681 <136687931+SoniyaPawar1681@users.noreply.github.com>
2023-06-21 20:30:51 +0530

    Create Program64.java

---
+++ Program64.java
@@ -0,0 +1,22 @@
+package user1;
+import java.util.*;
+public class addition_number 
+{
+
+	public static void main(String[] args) 
+	{
+	 int a,b,c,d,e;
+     Scanner s1 = new Scanner(System.in);
+     System.out.println("Enter value of A:");
+     a=s1.nextInt();
+     System.out.println("Enter value of B:");
+     b=s1.nextInt();
+     System.out.println("Enter value of C:");
+     c=s1.nextInt();
+     System.out.println("Enter value of D:");
+     d=s1.nextInt();
+     e=a+b+c+d;
+     System.out.println("Addition Of five number:"+e);
+	}
+
+}
