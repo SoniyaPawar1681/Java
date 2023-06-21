fd7ddc9fe7ada623c03f83a0356a3e383599c356
SoniyaPawar1681 <136687931+SoniyaPawar1681@users.noreply.github.com>
2023-06-21 18:47:21 +0530

    Create Program17.java

---
+++ Program17.java
@@ -0,0 +1,18 @@
+package Oops_Concept;
+class sample
+{
+	public void sample(int x,int y)
+	{
+		System.out.println(x+y);
+	}
+}
+public class Parameterized_method {
+
+	public static void main(String[] args)
+	{
+		sample s1=new sample();
+		s1.sample(2, 3);
+				
+	}
+
+}
