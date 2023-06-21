6e61a80d62323eeee81366c84ffd1fd5c174f300
SoniyaPawar1681 <136687931+SoniyaPawar1681@users.noreply.github.com>
2023-06-21 18:35:16 +0530

    Create Program1.java

---
+++ Program1.java
@@ -0,0 +1,25 @@
+package Basic;
+import java.util.*;
+public class else_if {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		if(a==b)
+		{
+			System.out.println("equal");
+			
+		}
+		else if(a>b)
+		{
+			System.out.println("greater");
+		}
+		else
+		{
+			System.out.println("lesser");
+		}
+	}
+
+}
