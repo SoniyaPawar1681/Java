0520870c55ea02317152ffa7698ce2720b1cd8fa
SoniyaPawar1681 <136687931+SoniyaPawar1681@users.noreply.github.com>
2023-06-21 19:59:34 +0530

    Create Program33.java

---
+++ Program33.java
@@ -0,0 +1,18 @@
+package collectionfreamework1;
+import java.util.*;
+public class Hashtable1 
+{
+
+	public static void main(String[] args)
+	{
+		Hashtable<Integer,String> h1=new Hashtable<Integer,String>();
+	       h1.put(23,"Soniya");
+	       h1.put(45,"Sangram");
+	       h1.put(34,"Ankita");
+	       h1.put(67,"Priyanka");
+	       h1.put(11,"Aniket");
+	       System.out.println(h1);
+
+	}
+
+}
